492e2eccce51ac01731f11e44823f99c37b42ac3
thatpythonkid-sketch <77459947+thatpythonkid-sketch@users.noreply.github.com>
2021-01-21 11:24:04 +0200

    Update dice_roller.py
    added the veriable dice size, which asks the users for the number of sizes(number) that the dice they want to roll, and i also tweaked the veriable 'dice_rolls' to get an input from the user instead of being set to 2 every time. this is actually the number of dices that the program will roll.

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
 
 def main():
-  dice_rolls = 2
+  dice_rolls = int(input("how many dice would you like to roll? "))
   dice_sum = 0
+  dice_size = int(input("how many size are the dice? "))
   for i in range(0, dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum += roll
-    print(f'You rolled a {roll}')
+    if roll == 1:
+      print(f"you rolled a {roll}! critical death")
+    elif roll == dice_size:
+      print(f"you rolled a {roll}! critical succees")
+    else:
+      print(f"you rolled a {roll}")
   print(f"you have rolled a total of {dice_sum}")
 if __name__== "__main__":
   main()
